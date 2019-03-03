aa21058b482d191fcedce58b09f739a7a357318b
Scott Wilde <scottpaulwilde@gmail.com>
2019-03-02 23:41:05 -0800

    apr rename

--- apr-number.js
+++ apr-number.js
@@ -1,25 +1,42 @@
 const fs = require('fs');
 
-let path = `D:\\googledrive\\Orders\\APR\\4x7\\2`;
+let path = `G:\\scan\\collected works\\out`;
 
 let filenames = fs.readdirSync(path);
 
 let files = [];
-let re = /APR\s(\d+).+-(\d+)_(\d)/;
+//CW_DO_054_2R
+let re = /CW_DO_(\d\d\d)_(\d)[RL]/;
 filenames.forEach(filename => {
     //console.log(filename);
+
+    if (!filename.match(re)) return;
+
+    let m = re.exec(filename);
+
     let o = {
-        filename: filename
+        filename: filename,
+        scan: +m[1],
+        side: +m[2]
     };
 
     files.push(o);
+});
 
+files.sort((a, b) => {
+
+    if (a.scan < b.scan) return -1;
+    if (a.scan > b.scan) return 1;
+    if (a.side < b.side) return -1;
+    if (a.side > b.side) return 1;
+
+    return 0;
 });
 
 
 
 
-let pageNumber = 1;
+let pageNumber = 75;
 
 files.forEach(file => {
 
@@ -27,11 +44,11 @@ files.forEach(file => {
     if (page.length < 3) page = "0" + page;
     if (page.length < 3) page = "0" + page;
 
-    file.renameTo = page + ".png";
+    file.renameTo = page + ".tif";
 
     console.log(file.filename + " -> " + file.renameTo);
 
-    fs.renameSync(path + "\\" + file.filename, path + "\\2-" + file.renameTo);
+    fs.renameSync(path + "\\" + file.filename, path + "\\" + file.renameTo);
 
     pageNumber++;
 });
