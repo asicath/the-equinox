5b81846b7144ef05f8b0dac060335fc27e5b4aed
Scott Wilde <scottpaulwilde@gmail.com>
2020-05-03 13:42:10 -0700

    MAGICK section 4

---
+++ books/magick.js
@@ -0,0 +1,222 @@
+exports.data = {
+    title: 'MAGICK',
+    subtitle: 'IN THEORY AND PRACTICE',
+    pubName: 'Lecram Press',
+    pubLocation: 'Paris',
+    pubYear: '1929',
+    folder: 'book4/3',
+    creditFilename: 'credits-magick.pdf',
+    images: [
+        'magick4_cover.jpg'
+    ],
+    links: [
+        {text: 'info', url: 'https://www.100thmonkeypress.com/biblio/acrowley/books/magick_in_theory_1_1929/magick_in_theory_1_1929.htm'}
+    ],
+    contents: [
+
+        {
+            title: 'COVER - SECTION VI, PRINTER MARK',
+            pageStart: 438,
+            pageEnd: 438,
+            addPage: [
+                {filename: '_cover.jpg'}
+            ],
+        },
+
+        {
+            title: 'A FEW OF THE PRINCIPAL CORRESPONDENCES OF THE QABALAH',
+            subtitle: 'REPRINTED WITH ADDITIONS FROM 777',
+            pageStart: 303,
+            pageEnd: 324
+        },
+        {
+            title: 'Grimorium Sanctissimum',
+            pub: 'oto',
+            pageStart: 325,
+            pageEnd: 326
+        },
+        {
+            title: 'LIBER XXV, THE STAR RUBY',
+            pageStart: 327,
+            pageEnd: 327,
+            pub: 'aa',
+            pubClass: 'd',
+            number: 25,
+            grade: 0
+        },
+        {
+            title: 'LIBER XXXVI, THE STAR SAPPHIRE',
+            pageStart: 328,
+            pageEnd: 328,
+            pub: 'aa',
+            pubClass: 'd',
+            number: 36,
+            grade: 2
+        },
+        {
+            title: 'LIBER XLIV, THE MASS OF THE PHOENIX',
+            pageStart: 329,
+            pageEnd: 330,
+            pub: 'aa',
+            pubClass: 'd',
+            number: 44,
+            grade: 6
+        },
+        {
+            title: 'LIBER V vel REGULAI',
+            pageStart: 331,
+            pageEnd: 344,
+            pub: 'aa',
+            pubClass: 'd',
+            number: 5,
+            grade: [0,1,2,3,4,5,6]
+        },
+        {
+            title: 'LIBER XV, O. T. O., ECCLESIÆ GNOSTICÆ CATHOLICÆ, CANON MISSÆ',
+            pageStart: 345,
+            pageEnd: 361,
+            pub: 'oto',
+            number: 15
+        },
+        {
+            title: 'LIBER HHH',
+            subtitle: 'SUB FIGURA CCCXLI',
+            pageStart: 362,
+            pageEnd: 367,
+            pub: 'aa',
+            pubClass: 'd',
+            number: 341,
+            grade: [2]
+        },
+        {
+            title: 'LIBER E, vel EXERCITIORUM',
+            subtitle: 'SUB FIGURA IX',
+            pageStart: 368,
+            pageEnd: 374,
+            pub: 'aa',
+            pubClass: 'b',
+            number: 9,
+            grade: [0, 1],
+        },
+        {
+            title: 'LIBER O vel MANUS ET SAGITTÆ',
+            subtitle: 'SUB FIGURA VI',
+            pageStart: 375,
+            pageEnd: 389,
+            addPage: [
+                {filename: '374image.png', before: '375.png'}
+            ],
+            pub: 'aa',
+            pubClass: 'b',
+            number: 6,
+            grade: [0, 1]
+        },
+        {
+            title: 'LIBER ASTARTE vel BERYLLI',
+            pageStart: 390,
+            pageEnd: 404,
+            pub: 'aa',
+            pubClass: 'd',
+            number: 175,
+            grade: [4]
+        },
+        {
+            title: 'LIBER RV vel SPIRITUS',
+            subtitle: 'SUB FIGURA CCVI',
+            pageStart: 405,
+            pageEnd: 409,
+            pub: 'aa',
+            pubClass: 'd',
+            number: 206,
+            grade: [2]
+        },
+        {
+            title: 'LIBER YOD',
+            subtitle: 'SUB FIGURA DCCCXXI',
+            pageStart: 410,
+            pageEnd: 414,
+            pub: 'aa',
+            pubClass: 'd',
+            number: 831,
+            grade: [5]
+        },
+        {
+            title: 'LIBER תישארב vel THISHARB',
+            subtitle: 'SUB FIGURA CMXIII',
+            pageStart: 415,
+            pageEnd: 422,
+            pub: 'aa',
+            pubClass: 'b',
+            number: 913,
+            grade: [2],
+            filename: 'liber-thisharb'
+        },
+
+
+        {
+            title: 'LIBER B vel MAGI',
+            subtitle: 'SUB FIGURA I',
+            pageStart: 423,
+            pageEnd: 424,
+            pub: 'aa',
+            pubClass: 'a',
+            number: 1,
+            grade: [6]
+        },
+        {
+            title: 'LIBER RESH vel HELIOS',
+            subtitle: 'SUB FIGURA CC',
+            pageStart: 425,
+            pageEnd: 426,
+            pub: 'aa',
+            pubClass: 'd',
+            number: 200,
+            grade: [0]
+        },
+        {
+            title: 'LIBER III vel JUGORUM',
+            pageStart: 427,
+            pageEnd: 429,
+            pub: 'aa',
+            pubClass: 'd',
+            number: 3,
+            grade: [2]
+        },
+        {
+            title: 'LIBER CHETH vel VALLUM ABIEGNI',
+            subtitle: 'SUB FIGURA CLVI',
+            pageStart: 430,
+            pageEnd: 431,
+            pub: 'aa',
+            pubClass: 'a',
+            number: 156,
+            grade: [6]
+        },
+        {
+            title: 'LIBER AʼASH CAPRICORNI PNEUMATICI',
+            subtitle: 'SUB FIGURA CCCLXX',
+            pageStart: 432,
+            pageEnd: 434,
+            pub: 'aa',
+            pubClass: 'a',
+            number: 370,
+            grade: [5],
+            filename: 'liber-aash-capricorni-pneumatici'
+        },
+        {
+            title: 'LIBER A vel ARMORUM',
+            subtitle: 'SUB FIGURA CCCXII',
+            pageStart: 435,
+            pageEnd: 436,
+            pub: 'aa',
+            pubClass: 'd',
+            number: 412,
+            grade: [1]
+        }
+    ],
+    publish: {
+        instructions:[
+
+        ]
+    }
+};
